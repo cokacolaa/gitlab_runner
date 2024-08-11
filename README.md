c89bfa8a8246a181bd7d745ac63cfa964a28a79f
DyLan <121837684+dylansys@users.noreply.github.com>
2024-08-11 21:35:09 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# gitlab_runner
+# Install CLI
+
+Clone then edit install-gitlab-runner.sh file
+
+   ```
+git clone https://github.com/tu4n2510/gitlab-runner.git && cd ./gitlab-runner && chmod +x install-gitlab-runner.sh && vim install-gitlab-runner.sh
+   ```
+
+Run the file
+
+   ```
+./install-gitlab-runner.sh
+   ```
---
+++ install-gitlab-runner.sh
@@ -0,0 +1,13 @@
+#run the compose file
+docker-compose up -d
+
+#register the runner inside the container
+docker-compose exec gitlab-runner \
+gitlab-runner register \
+--non-interactive \
+--url CHANGEME \
+--registration-token CHANGEME \
+--executor shell \
+--description "CHANGEME" \
+--docker-image "gitlab/gitlab-runner:alpine" \
+--docker-volumes /var/run/docker.sock:/var/run/docker.sock
